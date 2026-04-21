961eaab978cc56b5e8b3127a2ae4ff7132f85f54
Roman <remesloroman@gmail.com>
2026-04-21 23:05:33 +0200

    delete redundant comments

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,26 +1,7 @@
-
-/*
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/App.jsx'
-import "./styles/index.css";
-import { AuthProvider } from './context/AuthContext.jsx';
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
-)
-
- */
-
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.jsx'
+import "./styles/index.css";
 import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
