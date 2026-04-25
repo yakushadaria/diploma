d1034dd2010aa106c13b3628814b028e1facd149
Roman <remesloroman@gmail.com>
2026-04-25 20:12:35 +0200

    Refactor folders

--- src/frontend/src/components/CourseCard/CourseCard.jsx
+++ src/frontend/src/components/CourseCard/CourseCard.jsx
@@ -1,30 +1,3 @@
-
-/*
-import "./CourseCard.css";
-
-
-function CourseCard({ title, language, level, description }) {
-  return (
-    <div className="card">
-      <h2 className="title">{title}</h2>
-
-      <div className="meta">
-        <span>🌍 {language}</span>
-        <span>📊 {level}</span>
-      </div>
-
-      <p className="desc">{description}</p>
-    </div>
-  );
-}
-
-export default CourseCard;
-
- */
-
-
-
-
 import "./CourseCard.css";
 import { Link } from "react-router-dom";
 
